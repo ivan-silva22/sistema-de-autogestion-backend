f88d768ed781eaf0c82cb65ca8b36100e44dd24c
Ivan Silva <silvaivan280@gmail.com>
2024-08-01 18:53:52 -0300

    agregue el controlador para eliminar los datos de los examenes

--- src/controllers/examenes.controllers.js
+++ src/controllers/examenes.controllers.js
@@ -2,18 +2,32 @@ import Examen from "../models/examen";
 
 export const inscripcionExamenFinal = async (req, res) =>{
     try {
-        const { nombreMateria } = req.body;
-        let examen = await Examen.findOne({nombreMateria});
-        if(examen){
-            return res.status(400).json({
-                mensaje: "Ya existe el nombre de la materia"
-            })
-        }
-        examen = new Examen(req.body);
-        await examen.save();
-        res.status(201).json({
-            mensaje: "los examenes ya se guardo en la base de datos correctamente"
-        })
+        const { nombreMateria, alumnosInscriptos } = req.body;
+        let examen = await Examen.findOne({ nombreMateria });
+        if (examen) {
+            const alumno = alumnosInscriptos[0];    
+            if (!examen.alumnosInscriptos.some(
+              (inscripto) => inscripto.dni === alumno.dni
+            )) {
+              examen.alumnosInscriptos.push(alumno);
+              await examen.save();
+            } else {
+              return res.status(200).json({
+                mensaje: "El alumno ya está inscrito en este examen",
+              });
+            }
+          } else {
+            examen = new Examen({
+              nombreMateria,
+              fecha: req.body.fecha, 
+              alumnosInscriptos,
+            });
+            await examen.save();
+          }
+      
+          res.status(201).json({
+            mensaje: "El examen se guardó correctamente en la base de datos",
+          });
     } catch (error) {
         console.log(error);
         res.status(404).json({
@@ -30,6 +44,20 @@ export const obtenerExamenesFinales = async (req, res) =>{
         console.log(error);
         res.status(404).json({
             mensaje: "Error al obtener los examenes finales"
+        })
+    }
+}
+
+export const eliminarDatosExamen = async(req, res) =>{
+    try {
+        await Examen.deleteMany({});
+        res.status(200).json({
+            mensaje: `Se eliminaron documentos de la base de datos.`,
+          });
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({
+            mensaje: "Error, no se puede eliminar los datos"
         })
     }
 }
--- src/routes/examen.routes.js
+++ src/routes/examen.routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
-import { inscripcionExamenFinal, obtenerExamenesFinales } from "../controllers/examenes.controllers";
+import {
+  eliminarDatosExamen,
+  inscripcionExamenFinal,
+  obtenerExamenesFinales,
+} from "../controllers/examenes.controllers";
 import validarExamen from "../helpers/validarExamen";
 
 const router = Router();
 
-router.route('/finales').post(validarExamen ,inscripcionExamenFinal).get(obtenerExamenesFinales);
-
+router
+  .route("/finales")
+  .post(validarExamen, inscripcionExamenFinal)
+  .get(obtenerExamenesFinales)
+router.route("/eliminar").delete(eliminarDatosExamen)
 export default router;
